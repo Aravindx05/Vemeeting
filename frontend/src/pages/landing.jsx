ab29765b8054ca8174da93730b1fb81de630dc80
Aravind <aravindreddy8900@gmail.com>
2025-12-11 17:39:33 +0530

    landingpage ui

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Landing() {
     return (
@@ -15,6 +16,23 @@ export default function Landing() {
                 </div>
             </div>
            </nav>
+           <div className="landingMainContainer">
+            <div>
+                <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved Ones</h1>
+                <p>Cover a distance by Vemeeting</p>
+                {/* <div>
+                    <button>Get Started</button>
+                </div> */}
+                <div>
+                    <Link to="/home">
+                        <button>Get Started</button>
+                    </Link>
+                </div>
+            </div>
+            <div>
+                <img src="/image1.png" alt="landingImage" />
+            </div>
+           </div>
         </div>
     );
 }
